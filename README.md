faadcf33a1fde2068869fd6b27c745db673c1c62
Luca Del Puppo <luca@delpuppo.net>
2023-06-11 09:29:59 +0200

    init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Typescript tips
+
+This repo contains some tips for Typescript to improve your code and use Typescript like a Pro.
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import typescriptLogo from './assets/typescript.png'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://www.typescriptlang.org/" target="_blank">
+          <img src={typescriptLogo} className="logo" alt="Typescript logo" />
+        </a>
+      </div>
+      <h1>Typescript Tips</h1>
+    </>
+  )
+}
+
+export default App
